e489483d20c5f266467c1abe86efeff2f3aaee48
Seungho Chae <danielc95@naver.com>
2018-01-28 11:40:38 +0900

    리스트

---
+++ LIST.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+struct employee{
+	char name[10];
+	int age;
+	int salary;
+	char department[20];
+	}Employee;
+
+void insertinfo(Employee*, char *){
+	
+}
+void deleteinfo(Employee*, char *){
+	
+}
+int findinfo(Employee*, char *){
+	
+}
+void info(Employee*, char *){
+	
+}
+int avgBenefit(Employee*, char *){
+	
+}
+int main(){
+
+
+	
+		
+	return 0;
+}
